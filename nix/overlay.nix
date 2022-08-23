73cf8f503cacf5f5b6508e18e9c734c0420dec71
Astro <astro@spaceboyz.net>
2022-08-24 00:57:35 +0200

    opensta: init

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,16 +1,19 @@
 final: prev:
-(with builtins; seq ((hasAttr "inputs" prev) || throw "If you are calling this directly, make sure the overlays have an `inputs` attribute conforming to the flakes interface."))
+let
+  inputVersion = input:
+    let
+      inherit (input) lastModifiedDate;
+      year = builtins.substring 0 4 lastModifiedDate;
+      month = builtins.substring 4 2 lastModifiedDate;
+      day = builtins.substring 6 2 lastModifiedDate;
+    in
+      "unstable-${year}-${month}-${day}";
+in (
+with builtins; seq ((hasAttr "inputs" prev) || throw "If you are calling this directly, make sure the overlays have an `inputs` attribute conforming to the flakes interface."))
 {
-  luna = prev.libsForQt5.callPackage ({ wrapQtAppsHook, qtbase, qttools, mkDerivation, python38 }: mkDerivation {
+    luna = prev.libsForQt5.callPackage ({ wrapQtAppsHook, qtbase, qttools, mkDerivation, python38 }: mkDerivation {
     pname = "luna";
-    version =
-      let
-        inherit (prev.inputs.luna) lastModifiedDate;
-        year = builtins.substring 0 4 lastModifiedDate;
-        month = builtins.substring 4 2 lastModifiedDate;
-        day = builtins.substring 6 2 lastModifiedDate;
-      in
-        "unstable-${year}-${month}-${day}";
+    version = inputVersion final.inputs.luna;
 
     src = final.inputs.luna;
 
@@ -25,9 +28,21 @@ final: prev:
         --replace 'DESTINATION ''${LUNA_INSTALL_PREFIX}/bin)' "DESTINATION $out/bin)"
       substituteInPlace test/core/CMakeLists.txt \
         --replace 'enable_testing()' '''
+      substituteInPlace gui/src/mainwindow.cpp \
+        --replace 'settings.value("opensta_location", "/usr/local/bin/sta").toString()' 'QString("${final.opensta}/bin/sta")'
     '';
 
     # broken as tools/doctool is used before it is being built
     enableParallelBuilding = false;
+  }) {};
+
+  opensta = prev.callPackage ({ stdenv, cmake }: stdenv.mkDerivation {
+    pname = "opensta";
+    version = inputVersion final.inputs.opensta;
+
+    src = final.inputs.opensta;
+
+    nativeBuildInputs = with prev; [ cmake flex bison swig ];
+    buildInputs = with prev; [ zlib tcl ];
   }) {};
 }
